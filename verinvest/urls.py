dae71a1482b6ebcde13f29e49edd741e9092d62b
Jeremy Alva Prathama <jeremyalvax@gmail.com>
2022-10-25 15:42:20 +0700

    minor updates

--- verinvest/urls.py
+++ verinvest/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landing_page.urls')),
     path('collections/', include('collection.urls')),
     path('profile/', include('profile_page.urls')),
-    path('about/', include('about.urls'))
+    path('about/', include('about.urls')),
 ]
