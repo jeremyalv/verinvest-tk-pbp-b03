aac154eca0824503267d03bc3b7a3db3ee11974d
aliccciao <alicia.utomo@gmail.com>
2022-11-02 23:02:40 +0700

    updating edit profile modals

--- profile_page/views.py
+++ profile_page/views.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from profile_page.models import Profile
-from django.contrib.auth.models import User
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from collection.models import Post
 
 @login_required(login_url="{% url 'landing_page:login' %}")
 def show_profile(request):
+    user = Profile.objects.get(user = request.user)
+    posts = Post.objects.filter(author = user)
+
     user_loggedin = False
 
     if request.user.is_authenticated:
         user_loggedin = True
     context = {
         'username' : request.user.username, 
-        'user_loggedin': user_loggedin
+        'user_loggedin': user_loggedin,
+        'posts': posts
     }
 
     return render(request, "profile.html", context)
@@ -29,3 +35,12 @@ def edit_profile(request):
         'user_loggedin' : user_loggedin
     }
     return render(request, "modify.html", context)
+
+@login_required(login_url="{% url 'landing_page:login' %}")
+def edit_name(request):
+    user_profile = Profile.objects.get(user = request.user)
+    user_profile.first_name = request.POST.get('first_name')
+    user_profile.last_name = request.POST.get('last_name')
+    user_profile.save()
+
+    return HttpResponseRedirect(reverse('show_profile'))
