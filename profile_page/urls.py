2934b635c0f8162d9a1346c5be89b374f9328dd6
aliccciao <alicia.utomo@gmail.com>
2022-12-11 18:29:29 +0700

    add edit first_name feature

--- profile_page/urls.py
+++ profile_page/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from profile_page.views import show_profile, edit_profile, get_profile_json
+from profile_page.views import show_profile, edit_profile, get_profile_json, edit_firstname
 
 app_name = 'profile_page'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', show_profile, name='show_profile'),
     path('edit/', edit_profile, name='edit_profile'),
     path('json/', get_profile_json, name='get_profile_json'),
+    path('first_name/', edit_firstname, name='edit_firstname'),
 ]
